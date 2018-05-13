2385b0a059951b3434b5d7517f6da1d12e358ccf
Laurent Roger <laurent@laurentroger.com>
2018-05-13 22:11:06 +0200

    fixes Alert proptype color instead of bsStyle

--- packages/vulcan-ui-reactstrap/lib/components/ui/Alert.jsx
+++ packages/vulcan-ui-reactstrap/lib/components/ui/Alert.jsx
@@ -3,6 +3,6 @@ import { Alert } from 'reactstrap';
 import { registerComponent } from 'meteor/vulcan:lib';
 
 const BootstrapAlert = ({ children, variant,  ...rest }) => 
-  <Alert bsStyle={variant} {...rest}>{children}</Alert>
+  <Alert color={variant} {...rest}>{children}</Alert>
 
 registerComponent('Alert', BootstrapAlert);
